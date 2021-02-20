526d603cece997d75c1830e3698e112ce35d8951
Ajakaiye Afeso <marcusitovie@gmail.com>
2021-02-20 23:54:13 +0100

    Add Procfile

--- app.js
+++ app.js
@@ -84,7 +84,11 @@ app.post("/request", function(req, res) {
 });
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
 app.listen(port, function(req, res) {
 	console.log(`Server started on port ${port}`);
 });
